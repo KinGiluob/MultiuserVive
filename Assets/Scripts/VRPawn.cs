b77b2708e78602bcaaf6049d1271e96d3e058d99
Pascal Auberson <pascal@lumacode.com>
2017-06-22 21:47:42 +0100

    Script API upgrade. Name VR Pawns so easier to tell which is local and remote players

--- Assets/Scripts/VRPawn.cs
+++ Assets/Scripts/VRPawn.cs
@@ -15,6 +15,10 @@ public class VRPawn : NetworkBehaviour {
             GetComponentInChildren<SteamVR_ControllerManager>().enabled = true;
             GetComponentsInChildren<SteamVR_TrackedObject>(true).ToList().ForEach(x => x.enabled = true);
             Head.GetComponentsInChildren<MeshRenderer>(true).ToList().ForEach(x => x.shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.ShadowsOnly);
+            gameObject.name = "VRPawn (LocalPlayer)";
+        } else
+        {
+            gameObject.name = "VRPawn (RemotePlayer)";
         }
 	}
 
